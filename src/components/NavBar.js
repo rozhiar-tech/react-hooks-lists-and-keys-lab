b935e1ffe89bffc1f6bea650807a783929c8eff5
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2022-11-20 23:44:18 +0300

    completed assigmente

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const newLinks = links.map((link) => {
+    return(
+      <a href={"#" + link} key={link}>{link}</a>
+    )
+  });
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{newLinks}</nav>;
 }
 
 export default NavBar;
